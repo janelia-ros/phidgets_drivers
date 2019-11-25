53ed93bb827173c5fdb4a8625c8bd15f54193cf2
Peter Polidoro <peterpolidoro@gmail.com>
2019-11-25 11:31:27 -0500

    Remove unnecessary base-class initialization

--- phidgets_api/src/phidget22.cpp
+++ phidgets_api/src/phidget22.cpp
@@ -31,7 +31,7 @@
 
 namespace phidgets
 {
-Phidget22Error::Phidget22Error(const std::string& msg, PhidgetReturnCode code) : std::exception()
+Phidget22Error::Phidget22Error(const std::string& msg, PhidgetReturnCode code)
 {
   const char* error_ptr;
   PhidgetReturnCode ret = Phidget_getErrorDescription(code, &error_ptr);
