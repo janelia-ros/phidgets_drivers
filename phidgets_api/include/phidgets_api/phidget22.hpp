12e88f8e625270eea0ee7027af10baf1d6124c23
Peter Polidoro <peterpolidoro@gmail.com>
2019-11-25 11:14:24 -0500

    Make sure what() method is marked as override

--- phidgets_api/include/phidgets_api/phidget22.hpp
+++ phidgets_api/include/phidgets_api/phidget22.hpp
@@ -51,7 +51,7 @@ class Phidget22Error final : public std::exception
 public:
   explicit Phidget22Error(const std::string& msg, PhidgetReturnCode code);
 
-  const char* what() const noexcept;
+  const char* what() const noexcept override;
 
 private:
   std::string msg_;
